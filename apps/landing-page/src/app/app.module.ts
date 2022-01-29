abe11e6a590faeca515689260c3528b508c0ac18
Hozefa Mangalorewala <hozefam@live.in>
2022-01-29 18:37:25 +0300

    updated readme

--- apps/landing-page/src/app/app.module.ts
+++ apps/landing-page/src/app/app.module.ts
@@ -1,11 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
